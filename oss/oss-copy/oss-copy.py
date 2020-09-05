4b93365971a18d83f151083f3e285d8c3ab984c1
chunhua.zch <chunhua.zch@B-K2MJLVDL-1650.local>
2020-09-05 21:13:18 +0800

    add

--- oss/oss-copy/oss-copy.py
+++ oss/oss-copy/oss-copy.py
@@ -2,29 +2,66 @@
 import oss2
 import csv
 import urlparse
+import subprocess
+import os
+import json
 
-access_key = 'LT4R'
-access_secret = 'f66S'
-inventory = 'bff9c70d-45c6-4dfe-98ca-e37faeb5a001.csv'
+# access key and secret
+access_key = 'LTR'
+access_secret = 'f6RD6S'
+# bucket name
 bucket_name = 'mybucket'
+# endpoint for bucket
 domain = 'http://oss-cn-beijing.aliyuncs.com'
+#bucket inventory menifest file path
+manifest_url = 'https://mybucket.oss-cn-beijing.aliyuncs.com/mybucket/all/2020-09-03T06-42Z/manifest.json'
 
-# 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-auth = oss2.Auth(access_key,  access_secret)
-# Endpoint以杭州为例，其它Region请按实际情况填写。
-bucket = oss2.Bucket(auth, domain, bucket_name)
+# proccess single csv file
+def process(inventory):
+  auth = oss2.Auth(access_key,  access_secret)
+  bucket = oss2.Bucket(auth, domain, bucket_name)
+  with open(inventory) as csv_file:
+      csv_reader = csv.reader(csv_file, delimiter=',')
+      line_count = 0
+      for row in csv_reader:
+          key = urlparse.unquote(row[1])
+          enc_status = row[7]
+          if int(row[2]) > 1073741824 :
+            print("file %s is too large." %key)
+            continue
+          if(enc_status == 'FALSE' or enc_status == 'false'):
+              bucket.copy_object(bucket_name, key, key)
+          line_count = line_count + 1
+          print(".")
+
+def main():
+  #down load manifest file 
+  menifest_file_name = manifest_url.split("/")[-1]
+  cmd='wget -O %s %s' % (menifest_file_name, manifest_url)
+  subprocess.call(cmd, shell=True)
+  print("success get menifest file: %s" %manifest_url)
+  # https://andybrier.oss-cn-beijing.aliyuncs.com'
+  host =  urlparse.urlparse(manifest_url).scheme + "://" + urlparse.urlparse(manifest_url).hostname
+  with open( menifest_file_name,'r') as menifest:
+        m_json = json.load(menifest)
+        for csv_file in m_json['files']:
+          local_gz_file = csv_file['key'].split("/")[-1]
+          down_csv_cmd = 'wget -O %s %s' % (local_gz_file, host + '/' + csv_file['key'])
+          subprocess.call(down_csv_cmd, shell=True)
+          #gzip
+          ugzip = 'gzip -d %s' %local_gz_file
+          subprocess.call(ugzip, shell=True)
+          inventory =  os.path.splitext(local_gz_file)[0]
+          print("start to proccess file: %s" %inventory)
+          process(inventory)
+          print("success proccessed file: %s" %inventory)
 
 
-with open(inventory) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-         key = urlparse.unquote(row[1])
-         enc_status = row[7]
-         if int(row[2]) > 1073741824 :
-           print("file %s is too large." %key)
-           continue
-         if(enc_status == 'FALSE' or enc_status == 'false'):
-             bucket.copy_object(bucket_name, key, key)
-         line_count = line_count + 1
-         print('%d' %line_count)
+
+if __name__ == "__main__":
+    main()
+
+
+		
+
+ 

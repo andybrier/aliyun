4e9b691c082d97e8c8e1712f26c53c182b090f6b
chunhua, zhang <andybrier@qq.com>
2020-09-07 11:39:38 +0800

    Update oss-copy.py

--- oss/oss-copy/oss-copy.py
+++ oss/oss-copy/oss-copy.py
@@ -16,10 +16,11 @@ domain = 'http://oss-cn-beijing.aliyuncs.com'
 #bucket inventory menifest file path
 manifest_url = 'https://mybucket.oss-cn-beijing.aliyuncs.com/mybucket/all/2020-09-03T06-42Z/manifest.json'
 
+auth = oss2.Auth(access_key,  access_secret)
+bucket = oss2.Bucket(auth, domain, bucket_name)
 # proccess single csv file
 def process(inventory):
-  auth = oss2.Auth(access_key,  access_secret)
-  bucket = oss2.Bucket(auth, domain, bucket_name)
+
   with open(inventory) as csv_file:
       csv_reader = csv.reader(csv_file, delimiter=',')
       line_count = 0
@@ -35,24 +36,20 @@ def process(inventory):
           print(".")
 
 def main():
-  #download manifest file 
+  #down load manifest file 
   menifest_file_name = manifest_url.split("/")[-1]
-  cmd='wget -O %s %s' % (menifest_file_name, manifest_url)
-  subprocess.call(cmd, shell=True)
+  bucket.get_object_to_file(urlparse.urlparse(manifest_url).path.lstrip('/'), menifest_file_name)
+
   print("success get menifest file: %s" %manifest_url)
   # https://andybrier.oss-cn-beijing.aliyuncs.com'
-  host =  urlparse.urlparse(manifest_url).scheme + "://" + urlparse.urlparse(manifest_url).hostname
   with open( menifest_file_name,'r') as menifest:
         m_json = json.load(menifest)
         for csv_file in m_json['files']:
-          #download inventory gzip file:  xxxx.csv.gz
           local_gz_file = csv_file['key'].split("/")[-1]
-          down_csv_cmd = 'wget -O %s %s' % (local_gz_file, host + '/' + csv_file['key'])
-          subprocess.call(down_csv_cmd, shell=True)
+          bucket.get_object_to_file(csv_file['key'], local_gz_file)
           #gzip
           ugzip = 'gzip -d %s' %local_gz_file
           subprocess.call(ugzip, shell=True)
-          #final inventory csv file: xxxx.csv
           inventory =  os.path.splitext(local_gz_file)[0]
           print("start to proccess file: %s" %inventory)
           process(inventory)
@@ -63,8 +60,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-		
-
- 
